72cf68a13840a09b7e02bbbfd579b549ddf70ae9
Domenico-dellaPERUTA <domenicodellaperuta@smartblue.it>
2024-09-18 23:24:51 +0200

    sfondo home

--- src/main.js
+++ src/main.js
@@ -10,18 +10,26 @@ const app =Vue.createApp({
   data() {
     return {
       titolo: "Rete Neurale",
-      messaggio : ''
+      messaggio : '',
+      step: 0,
     }
   },
   methods:{
     async creaRete(infoRete){
       // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+      this.next();
       this.messaggio = await invoke("crea_rete", { 
         apprendimento: infoRete.tasso_apprendimento, 
         attivazione:   infoRete.tipo_funz_attivazione,
         neuroni:       infoRete.neuroni.map( item => item.neuroni ),
         alfa:          infoRete.alfa
       });
+    },
+    async start(go){
+      if(go)this.step++;
+    },
+    next(){
+      this.step++;
     }
   }
 })
